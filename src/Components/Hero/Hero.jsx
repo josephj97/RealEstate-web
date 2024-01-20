0e1728610751e70e4e8fdfa8082ce55d1d7e0817
Joseph Atef <josephatef.j97@gmail.com>
2024-01-20 03:12:48 +0200

    Added the left section of Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,6 +6,18 @@ const Hero = () => {
         <section className="hero-wrapper">
             <div className="hero-container paddings innerWidth flexCenter">
 
+                {/* left section */}
+                <div className="hero-left flexColStart">
+                    <div className="hero-title">
+                        <div className="orange-circle"></div>
+                        <h1>Discover <br />Most Suitable<br />Property</h1>
+                    </div>
+                    <div className="hero-des flexColStart">
+                        <span>Find a variety of properties that suit you very easilty</span>
+                        <span>Forget all difficulties in finding a residence for you</span>
+                    </div>
+                </div>
+
                 {/* right section */}
                 <div className="hero-right">
                     <div className="image-container">
