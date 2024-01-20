89b33b87d9a8f67ea07aef2b836f6a9e9adb9e67
Joseph Atef <josephatef.j97@gmail.com>
2024-01-20 03:11:23 +0200

    Added the right section of Hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+const Hero = () => {
+    return (
+        <section className="hero-wrapper">
+            <div className="hero-container paddings innerWidth flexCenter">
+
+                {/* right section */}
+                <div className="hero-right">
+                    <div className="image-container">
+                        <img src="./public/hero-image.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
