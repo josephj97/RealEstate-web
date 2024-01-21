8683e1126ff5b875d4c4bbf3212ddbe758b19ffb
Joseph Atef <josephatef.j97@gmail.com>
2024-01-21 02:02:49 +0200

    Added Residencies component with some styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero"
 import './App.css'
 import Companies from "./Components/Companies/Companies";
+import Residencies from "./Components/Residencies/Residencies";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero></Hero>
       </div>
       <Companies></Companies>
+      <Residencies></Residencies>
     </div>
 
   );
---
+++ src/Components/Residencies/Residencies.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Residencies.css'
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import 'swiper/css'
+import data from '../../utils/slider.json'
+import { color } from 'framer-motion'
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+        <div className="r-container innerWidth paddings">
+            <div className="r-head flexColStart">
+                <span className='orangeText'>Best Choices</span>
+                <span className='primaryText'>Popular Residencies</span>
+            </div>
+            <Swiper>
+                {
+                    data.map((card,i)=> (
+                        <SwiperSlide key={i}>
+                            <div className="r-card flexColStart">
+                                <img src={card.image} alt='home'></img>
+                                <span className="secondaryText r-price">
+                                    <span style={{color: 'orange'}}>$</span><span>{card.price}</span>
+                                </span>
+                                <span className="primaryText">{card.name}</span>
+                                <span className="secondaryText">{card.detail}</span>
+                            </div>
+                        </SwiperSlide>
+                        
+                    ))
+                }
+            </Swiper>
+        </div>
+    </section>
+  )
+}
+
+export default Residencies
