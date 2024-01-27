950867ab6f5c7fd57c803f8a78ac2a021f3f4653
Joseph Atef <josephatef.j97@gmail.com>
2024-01-27 02:17:14 +0200

    Added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Residencies from "./Components/Residencies/Residencies";
 import Value from "./Components/Value/Value";
 import Contact from "./Components/Contact/Contact";
 import GetStarted from "./Components/GetStarted/GetStarted";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <Value></Value>
       <Contact></Contact>
       <GetStarted />
+      <Footer />
     </div>
 
   );
