d604121c36c6a29254101da4401666687b8e8fba
Joseph Atef <josephatef.j97@gmail.com>
2024-01-28 22:27:15 +0200

    Fixed the menu button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,35 +7,42 @@ const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false)
   const getMenuStyles = (menuOpened) => {
     if (document.documentElement.clientWidth <= 800) {
-      return { right: !menuOpened && "-100%" }
+      return { right: menuOpened && "4rem" }
     }
+  }
+  const handleMenuOpened = () => {
+    setMenuOpened((prev) => !prev);
   }
 
   return (
     <section className="h-wrapper">
       <div className="flexCenter innerWidth paddings h-container">
         <img src="./logo.png" alt="logo" width="100" />
-        
-        <OutsideClickHandler 
-        onOutsideClick={() => {
-          setMenuOpened(false)
-        }}
-        >
-          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
-            <a href="" className="">Residencies</a>
-            <a href="" className="">Our Value</a>
-            <a href="" className="">Contact Us</a>
-            <a href="" className="">Get Started</a>
-            <button className='button'>
-              <a href="" className="">Contact</a>
-            </button>
 
-          </div>
+        <OutsideClickHandler
+
+          onOutsideClick={() => {
+            setMenuOpened(false)
+          }}
+        >
+          <>
+            <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+              <a href="" className="">Residencies</a>
+              <a href="" className="">Our Value</a>
+              <a href="" className="">Contact Us</a>
+              <a href="" className="">Get Started</a>
+              <button className='button'>
+                <a href="" className="">Contact</a>
+              </button>
+
+            </div>
+            <div className="menu-icon" onClick={handleMenuOpened}>
+              <BiMenuAltRight size={30} />
+            </div>
+          </>
         </OutsideClickHandler>
 
-        <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
-          <BiMenuAltRight size={30} />
-        </div>
+
       </div>
     </section>
   )
